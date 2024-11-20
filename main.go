f14b086b99294a79a3edc98a7a0a7eafdfe81416
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-11-20 08:54:07 +0000

    gofmt

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "log"
-    "net/url"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
 
-    "golang.org/x/net/webdav"
+	"golang.org/x/net/webdav"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        log.Fatalf("[ERRO] Usage: http://hostname:port/path#directory")
-    }
-    rawURL := os.Args[1]
-    parsedURL, err := url.Parse(rawURL)
-    if err != nil {
-        log.Printf("[WARN] %v", err)
-    }
-    webdavHandler := &webdav.Handler{
-        FileSystem: webdav.Dir(parsedURL.Fragment),
-        Prefix:     parsedURL.Path,
-        LockSystem: webdav.NewMemLS(),
-    }
-    log.Printf("[INFO] %v", rawURL)
-    if err := http.ListenAndServe(parsedURL.Host, webdavHandler); err != nil {
-        log.Fatalf("[ERRO] %v", err)
-    }
+	if len(os.Args) < 2 {
+		log.Fatalf("[ERRO] Usage: http://hostname:port/path#directory")
+	}
+	rawURL := os.Args[1]
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		log.Printf("[WARN] %v", err)
+	}
+	webdavHandler := &webdav.Handler{
+		FileSystem: webdav.Dir(parsedURL.Fragment),
+		Prefix:     parsedURL.Path,
+		LockSystem: webdav.NewMemLS(),
+	}
+	log.Printf("[INFO] %v", rawURL)
+	if err := http.ListenAndServe(parsedURL.Host, webdavHandler); err != nil {
+		log.Fatalf("[ERRO] %v", err)
+	}
 }
